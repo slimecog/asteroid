3ec98f4498c8d9e8525ecc9168354936425bdeb9
slimecog <wlcjohnson@gmail.com>
2018-04-11 09:48:47 -0600

    adds index logic to MDD controller, adds asteroid search servoce start

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,4 +1,5 @@
 class MostDangerousDayController < ApplicationController
   def index
+    @asteroids = AsteroidSearch.new(params[:start_date], params[:end_date]).neos
   end
 end
--- spec/features/user_can_see_most_dangerous_day_spec.rb
+++ spec/features/user_can_see_most_dangerous_day_spec.rb
@@ -9,6 +9,13 @@ feature "user can see most dangerous day" do
 
       visit "/"
 
+      fill_in "start_date", with: "2018-01-01"
+      fill_in "end_date", with: "2018-01-07"
+      click_on "Determine Most Dangerous Day"
+
+      expect(current_path).to eq('/most_dangerous_day')
+      expect(page).to have_content('Most Dangerous Day')
+
     end
   end
 end
@@ -16,12 +23,6 @@ end
 
 # Feature: A guest user visits the root page and enters a start date and end date. The resulting page will display the most dangerous day in that range based on which day has the most `is_potentially_dangerous_asteroid` set to `true`. Given the scenario below, it should be January 1, 2018 with 3 potentially dangerous asteroids.
 #
-# And I enter "2018-01-01" into the start date
-# And I enter "2018-01-07" into the end date
-# And I click "Determine Most Dangerous Day"
-#
-# Then I should be on "/most_dangerous_day"
-# And I should see a heading for "Most Dangerous Day"
 # And I should see "January 1, 2018 - January 7, 2018"
 # And I should see a heading for the most dangerous day in that range "January 1, 2018"
 # And I should see 3 asteroids in that list
