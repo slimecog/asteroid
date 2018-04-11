a1c9681a17ed3ca55dec8ab2d4822446686ed7e4
slimecog <wlcjohnson@gmail.com>
2018-04-11 11:44:42 -0600

    add logic to break out asteroids of most dangerous day in range

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,5 +1,33 @@
 class MostDangerousDayController < ApplicationController
   def index
-    @asteroids = NASAAsteroids.new(params[:start_date], params[:end_date]).neos
+    most_dangerous_day
+    dates_in_range
   end
+
+  private
+    def dates_in_range
+      @start_date = Date.parse(params[:start_date])
+      @end_date = Date.parse(params[:end_date])
+    end
+
+    def find_in_range
+      (NASAAsteroids.new(params[:start_date], params[:end_date]).neos).flatten
+    end
+
+    def dangerous_asteroids
+      find_in_range.map do |asteroid|
+        a = []
+        if asteroid.dangerous?
+          a << asteroid
+        end
+      end.compact.flatten
+    end
+
+    def dates_by_danger
+      dangerous_asteroids.sort_by { |asteroid| asteroid.approach_date }
+    end
+
+    def most_dangerous_day
+      @asteroids = dates_by_danger.find_all { |asteroid| asteroid.approach_date == dates_by_danger.first.approach_date }
+    end
 end
--- app/models/asteroid.rb
+++ app/models/asteroid.rb
@@ -1,11 +1,17 @@
 class Asteroid
   attr_reader :name,
               :reference,
+              :approach_date,
               :danger
 
   def initialize(attrs = {})
-    @name = attrs[:name]
-    @reference = attrs[:neo_reference_id]
-    @danger = attrs[:is_potentially_hazardous_asteroid]
+    @name          = attrs[:name]
+    @reference     = attrs[:neo_reference_id]
+    @approach_date = attrs[:close_approach_data].first[:close_approach_date]
+    @danger        = attrs[:is_potentially_hazardous_asteroid]
+  end
+
+  def dangerous?
+    @danger == true
   end
 end
